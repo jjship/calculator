e3eeace27193bc89e5bda84d81167a4cef7e24f4
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-08-10 17:50:20 +0200

    work in progress

--- react-js/src/index.js
+++ react-js/src/index.js
@@ -2,3 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "./grid.css";
+
+function Button(props) {
+  return <button className={props.kind}>{props.value}</button>;
+}
+
+class Keyboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      values: [
+        1,
+        2,
+        3,
+        4,
+        5,
+        6,
+        7,
+        8,
+        9,
+        0,
+        ".",
+        "/",
+        "*",
+        "-",
+        "=",
+        "+",
+        "C",
+        "AC",
+      ],
+    };
+  }
+}
