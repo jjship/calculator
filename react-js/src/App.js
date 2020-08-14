956281aaa421c90a396fde80bd95fca5cea58aa0
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-08-14 17:48:44 +0200

    input as component

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Keyboard from "./Components/Keyboard";
+import { Input } from "./Components/Input";
 import "./app.css";
 import "./grid.css";
 
 function App() {
   return (
     <div className="wrapper" id="wrapper">
-      <input type="text" className="display" value="0" disabled />
+      <Input />
       <Keyboard />
     </div>
   );
---
+++ react-js/src/Components/Input.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Input = () => {
+  return (
+    <>
+      <input type="text" className="display" value="0" disabled />
+    </>
+  );
+};
