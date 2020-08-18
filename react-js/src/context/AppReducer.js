acbc931d1fa7acee04904e717bc6b20ff6680f83
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-08-18 18:34:08 +0200

    globalstate initialized

---
+++ react-js/src/context/AppReducer.js
@@ -0,0 +1,7 @@
+export default (state, action) => {
+  switch (action.type) {
+    // TODO actions
+    default:
+      return state;
+  }
+};
---
+++ react-js/src/context/GlobalState.js
@@ -0,0 +1,30 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+const initialState = {
+  inputStore: "0",
+  firstNum: null,
+  operator: null,
+  waitingForSecondNum: false,
+};
+
+export const GlobalContext = createContext(initialState);
+
+// TODO actions
+
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        inputStore: state.inputStore,
+        firstNum: state.firstNum,
+        operator: state.operator,
+        waitingForSecondNum: state.waitingForSecondNum,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
