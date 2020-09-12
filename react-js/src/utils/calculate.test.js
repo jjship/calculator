357b109bff784ea88cfd13342302f587af3c83f3
Jacek Skrzypek <jjskrzypek@gmail.com>
2020-09-12 18:14:58 +0200

    calculate tested

--- react-js/src/utils/calculate.test.js
+++ react-js/src/utils/calculate.test.js
@@ -1,4 +1,17 @@
 import { calculate } from './calculate';
-test('Should act the same as eval', () => {
+
+test('Returns 7 when adding 2 and 5', () => {
   expect(calculate('+', 2, 5)).toBe(7);
+});
+test('Returns -3 when substracting 5 from 2', () => {
+  expect(calculate('-', 2, 5)).toBe(-3);
+});
+test('Returns 10 when multiplying 2 times 5', () => {
+  expect(calculate('*', 2, 5)).toBe(10);
+});
+test('Returns 0.4 when dividing 2 by 5', () => {
+  expect(calculate('/', 2, 5)).toBe(0.4);
+});
+test('Returns secondNum when the operator is =', () => {
+  expect(calculate('=', 2, 5)).toBe(5);
 });
